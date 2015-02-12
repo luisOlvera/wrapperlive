5ddadcf5e1caa9259eeee5e51a6cd1badc6f5fca
luisOlvera <luis.olvera@tralix.com>
2015-02-12 09:21:24 -0600

    katta wrapper

---
+++ src/com/clss/WordWrap.java
@@ -0,0 +1,12 @@
+package com.clss;
+
+/**
+ * Created by luisolvera on 11/02/15.
+ */
+public class WordWrap {
+
+    public void  wrapper () {
+
+
+    }
+}
---
+++ src/com/test/WordWrapTest.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import com.clss.WordWrap;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class WordWrapTest {
+    WordWrap wordWrap;
+    String cadena;
+    int width;
+
+    @Before
+    public void setUp() throws Exception {
+         wordWrap = new WordWrap();
+        cadena="";
+        width=1;
+
+    }
+
+
+    @Test
+    public void  noting()throws Exception {
+
+    }
+
+}
